6bd2f28bd2c5e146afb1f539b918ac9798c9aacd
konan ruth emmanuelle <konanruthemmanuelle@gmail.com>
2025-03-11 17:08:59 +0900

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials,messaging
 cred = credentials.Certificate("serviceAccountKey.json")
 
 default_app = firebase_admin.initialize_app(cred)
-registration_token = 'fRUE3jhfSKiyIwVhbhvVkm:APA91bGzwsvqF5OuUoVA7OrNc-S0LQKCmTEHoIk5_JPn8CI_d9LWdpf7bXi8olvXhH7FB4xVndPmUqQBypOxViXjBRa0BU3AO-73z1-cug465SCzL57KCOivH5pdQ_mRLYEyxoHpEIM4'
+registration_token = 'token'
 
     # See documentation on defining a message payload.
 message = messaging.Message(
