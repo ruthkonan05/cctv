de43ad7c3efe672d947dc62f345b8a250bcdd9d7
konan ruth emmanuelle <konanruthemmanuelle@gmail.com>
2025-03-11 17:11:20 +0900

    Update addDataToDatabase.py

--- addDataToDatabase.py
+++ addDataToDatabase.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    "databaseURL":"https://prof-kim-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    "databaseURL":"databaseurl_firebase"
 })
 ref = db.reference("personinfo")
 data={
